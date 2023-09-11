19a70eb8cc6ebe4dd5edc27c2d0a9f5504b9a8ae
kyusikko <kokyusik91@naver.com>
2023-09-11 07:22:46 -0700

    ✨ 인풋 컴포넌트와 Register-Modal 컴포넌트 생성

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -8,6 +8,9 @@ import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
+import Heading from '../Heading';
+import Input from '../inputs/Input';
+import { toast } from 'react-hot-toast';
 
 function RegisterModal() {
   const registerModal = useRegisterModal();
@@ -19,8 +22,8 @@ function RegisterModal() {
     formState: { errors },
   } = useForm<FieldValues>({
     defaultValues: {
-      name: '',
       email: '',
+      name: '',
       password: '',
     },
   });
@@ -34,12 +37,43 @@ function RegisterModal() {
         registerModal.onClose();
       })
       .catch((error: unknown) => {
-        console.log(error);
+        toast.error('Something Went Wrong');
       })
       .finally(() => {
         setIsLoading(false);
       });
   };
+
+  const bodyContent = (
+    <div className='flex flex-col gap-4'>
+      <Heading title='Welcome to Airbnb' subtitle='Create an Account!' />
+      <Input
+        id='email'
+        label='Email'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id='name'
+        label='Name'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id='password'
+        type='password'
+        label='Password'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
 
   return (
     <Modal
@@ -49,6 +83,7 @@ function RegisterModal() {
       actionLabel='Continue'
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
     />
   );
 }
