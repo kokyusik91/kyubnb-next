25e8ba3bfe905c817045cbd38341b41f92985435
kyusikko <kokyusik91@naver.com>
2023-09-12 22:33:27 -0700

    ✨ 로그인 모달을 배치하고 로그인 기능 추가! (next-auth-credentials)

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import ClientOnly from './components/ClientOnly';
 import Modal from './components/modals/Modal';
 import RegisterModal from './components/modals/RegisterModal';
 import ToasterProvider from './providers/ToasterProvider';
+import LoginModal from './components/modals/LoginModal';
 
 const font = Nunito({ subsets: ['latin'] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
           {/* <Modal actionLabel='Submit' title='hello world' isOpen={true} /> */}
           <NavBar />
