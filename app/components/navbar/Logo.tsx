c723dfd3be857d1c1ed2345ceba0de8fe7c1bb65
kyusikko <kks@adenasoft.com>
2023-09-15 12:34:16 +0900

    ✨ 로고를 눌렀을때는 쿼리 스트링 비우기

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -7,6 +7,7 @@ function Logo() {
   const router = useRouter();
   return (
     <Image
+      onClick={() => router.push('/')}
       alt='logo'
       className='hidden md:block cursor-pointer'
       width='100'
