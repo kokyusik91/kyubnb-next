d0a874f443d787cead9a4fa8305b56041606ca3d
kyusikko <kks@adenasoft.com>
2023-09-15 14:31:09 +0900

    ✨ 로그인 모달 & 회원가입 모달 열고 닫힘

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -20,8 +20,10 @@ import { useRouter } from 'next/navigation';
 
 function LoginModal() {
   const router = useRouter();
+
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
+
   const [isLoading, setIsLoading] = useState(false);
 
   const {
@@ -53,6 +55,11 @@ function LoginModal() {
       }
     });
   };
+
+  const toggle = useCallback(()=>{
+    loginModal.onClose();
+    registerModal.onOpen()
+  },[loginModal, registerModal])
 
   const bodyContent = (
     <div className='flex flex-col gap-4'>
@@ -94,12 +101,12 @@ function LoginModal() {
       />
       <div className='text-neutral-500 text-center mt-4 font-light'>
         <div className='justify-center flex flex-row items-center gap-2'>
-          <div>Already have an account?</div>
+          <div>First time using Airbnb?</div>
           <div
-            onClick={registerModal.onClose}
+            onClick={toggle}
             className='text-neutral-800 cursor-pointer hover:underline'
           >
-            Log in
+            Create an account
           </div>
         </div>
       </div>
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -6,6 +6,7 @@ import { FcGoogle } from 'react-icons/fc';
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
+import useLoginModal from '@/app/hooks/useLoginModal';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
@@ -16,6 +17,7 @@ import { signIn } from 'next-auth/react';
 
 function RegisterModal() {
   const registerModal = useRegisterModal();
+  const loginModal = useLoginModal()
   const [isLoading, setIsLoading] = useState(false);
 
   const {
@@ -45,6 +47,12 @@ function RegisterModal() {
         setIsLoading(false);
       });
   };
+
+
+  const toggle = useCallback(()=>{
+    registerModal.onClose();
+    loginModal.onOpen()
+  },[loginModal, registerModal])
 
   const bodyContent = (
     <div className='flex flex-col gap-4'>
@@ -96,7 +104,7 @@ function RegisterModal() {
         <div className='justify-center flex flex-row items-center gap-2'>
           <div>Already have an account?</div>
           <div
-            onClick={registerModal.onClose}
+            onClick={toggle}
             className='text-neutral-800 cursor-pointer hover:underline'
           >
             Log in
