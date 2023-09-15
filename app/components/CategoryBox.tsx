5027d87da31871489545a00b5d9da879e3592c43
kyusikko <kks@adenasoft.com>
2023-09-15 12:34:02 +0900

    ✨ 카테고리 박스 만들기 및 각각의 카테고리를 눌렀을때 쿼리 스트링으로 push 하는 기능 개발

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useCallback } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { IconType } from "react-icons";
@@ -5,14 +7,15 @@ import qs from "query-string";
 
 interface CategoryBoxProps {
     label : string;
-    description : string;
+    // description : string;
     icon: IconType
     selected ? : boolean
 }
 
-function CategoryBox({label, description, icon : Icon, selected} : CategoryBoxProps){
+function CategoryBox({label, icon : Icon, selected} : CategoryBoxProps){
     const router = useRouter();
     const params = useSearchParams();
+    console.log(params);
 
     const handleClick = useCallback(()=>{
         let currentQuery = {};
@@ -25,9 +28,24 @@ function CategoryBox({label, description, icon : Icon, selected} : CategoryBoxPr
             ...currentQuery, 
             category : label
         }
-    },[])
+        // 현재 쿼리 스트링의 category 값과 누른 label이 같으면 쿼리 삭제
+       if(params?.get('category') === label){
+        delete updateQuery.category;
+       }
+
+       // 실제로 이동시킬 쿼리 생성
+       const url = qs.stringifyUrl({
+        url : '/',
+        query : updateQuery
+       }, {skipNull : true})
+
+       router.push(url);
+       
+    },[label, params, router])
+
+
     return(
-        <div className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${selected ? 'border-b-neutral-800' : 'border-transparent'} ${selected ? 'text-neutral-800' : 'text-neutral-500'}`}>
+        <div onClick={handleClick} className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${selected ? 'border-b-neutral-800' : 'border-transparent'} ${selected ? 'text-neutral-800' : 'text-neutral-500'}`}>
             <Icon size={26} />
             <div className="font-medium text-sm">
                 {label}
--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,10 +1,14 @@
 'use client'
 
 import Container from "../Container"
-import {TbBeach} from 'react-icons/tb'
-import {GiWindmill} from 'react-icons/gi'
+import {TbBeach, TbMountain, TbPool} from 'react-icons/tb'
+import {BsSnow} from 'react-icons/bs'
+import {GiBarn, GiBoatFishing, GiCactus, GiCastle, GiCaveEntrance, GiForestCamp, GiIsland, GiWindmill} from 'react-icons/gi'
+import {FaSkiing} from 'react-icons/fa'
+import {IoDiamond} from 'react-icons/io5'
 import {MdOutlineVilla} from 'react-icons/md'
 import CategoryBox from "../CategoryBox"
+import { usePathname, useSearchParams } from "next/navigation"
 
 
 export const categories = [
@@ -19,19 +23,90 @@ export const categories = [
         description : "This property has windmills"
     },
     {
-        label : 'Windmills',
+        label : 'Modern',
         icon : MdOutlineVilla,
         description : "This property is modern"
-    }
+    },
+    {
+        label : 'Countryside',
+        icon : TbMountain,
+        description : "This property is in countryside!"
+    },
+    {
+        label : 'Pools',
+        icon : TbPool,
+        description : "This property has a pool"
+    },
+    {
+        label : 'Islands',
+        icon : GiIsland,
+        description : "This property is on an island"
+    },
+    {
+        label : 'Lake',
+        icon : GiBoatFishing,
+        description : "This property is close to a lake!"
+    },
+    {
+        label : 'Skiing',
+        icon : FaSkiing,
+        description : "This property has skiing activities"
+    },
+    {
+        label : 'Castles',
+        icon : GiCastle,
+        description : "This property is in a castle!"
+    },
+    {
+        label : 'Camping',
+        icon : GiForestCamp,
+        description : "This property has camping activities"
+    },
+    {
+        label : 'Arctic',
+        icon : BsSnow,
+        description : "This property has camping activities"
+    },
+    {
+        label : 'Cave',
+        icon : GiCaveEntrance,
+        description : "This property is in a cave!"
+    },
+    {
+        label : 'Desert',
+        icon : GiCactus,
+        description : "This property is in the desert"
+    },
+    {
+        label : 'Barns',
+        icon : GiBarn,
+        description : "This property is in the barn!"
+    },
+    {
+        label : 'Lux',
+        icon : IoDiamond,
+        description : "This property is luxxurious"
+    },
+    
 ]
 
 
 function Categories(){
+    const params = useSearchParams();
+    const categoryValue = params?.get('category');
+    const pathname = usePathname();
+
+    const isMainPage = pathname === '/'
+
+    if(!isMainPage){
+        return null
+    }
+
     return(
         <Container>
             <div className="pt-4 flex flex-row imtes-center justify-between overflow-x-auto">
                 {categories.map((item) => (
-                    <CategoryBox key={item.label} label={item.label} description={item.description} icon={item.icon}/>
+                    <CategoryBox key={item.label} label={item.label} selected={categoryValue === item.label} icon={item.icon}/>
                 ))}
             </div>
         </Container>
