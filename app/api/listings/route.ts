d310e5c16acc41e7219a8249c2a8fcb03de9197a
kyusikko <kokyusik91@naver.com>
2023-09-23 12:41:18 +0900

    ✨ RentModal에서 입력받은 데이터들을 실제 DB에 저장하는 로직

---
+++ app/api/listings/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from 'next/server';
+
+import prisma from '@/app/libs/prismadb';
+import getCurrentUser from '@/app/actions/getCurrentUser';
+
+export async function POST(request: Request) {
+  // 현재 로그인 되어있는 사용자의 정보가져오기
+  const currentUser = await getCurrentUser();
+  // 없으면 에러
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+  // 클라이언트에서 받아온 정보
+  const body = await request.json();
+  const {
+    category,
+    location,
+    guestCount,
+    roomCount,
+    bathroomCount,
+    imageSrc,
+    price,
+    title,
+    description,
+  } = body;
+
+  // You can remove this if you want to
+  Object.keys(body).forEach((value: any) => {
+    if (!body[value]) {
+      NextResponse.error();
+    }
+  });
+
+  const listing = await prisma.listing.create({
+    data: {
+      category,
+      locationValue: location.value,
+      guestCount,
+      roomCount,
+      bathroomCount,
+      imageSrc,
+      price: parseInt(price, 10),
+      title,
+      description,
+      userId: currentUser.id,
+    },
+  });
+
+  return NextResponse.json(listing);
+}
--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,10 +7,16 @@ import useRentModal from '@/app/hooks/useRentModal';
 import Heading from '../Heading';
 import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
-import { FieldValues, useForm } from 'react-hook-form';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import CountrySelect from '../inputs/CountrySelect';
 import dynamic from 'next/dynamic';
 import Counter from '../inputs/Counter';
+import ImageUpload from '../inputs/ImageUpload';
+import { ST } from 'next/dist/shared/lib/utils';
+import Input from '../inputs/Input';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 enum STEPS {
   CATEGORY = 0,
@@ -22,9 +28,11 @@ enum STEPS {
 }
 
 function RentModal() {
+  const router = useRouter();
   const rentModal = useRentModal();
 
   const [step, setStep] = useState(STEPS.CATEGORY);
+  const [isLoading, setIsLoading] = useState(false);
 
   const {
     register,
@@ -52,6 +60,7 @@ function RentModal() {
   const guestCount = watch('guestCount');
   const roomCount = watch('roomCount');
   const bathroomCount = watch('bathroomCount');
+  const imageSrc = watch('imageSrc');
 
   const Map = useMemo(
     () => dynamic(() => import('../Map'), { ssr: false }),
@@ -71,6 +80,37 @@ function RentModal() {
 
   const onNext = () => {
     setStep((prev) => prev + 1);
+  };
+
+  /**
+   * 실제 작성한 Form들을 제출하는 함수이다.
+   * @param data 폼에 포함된 데이터
+   * @returns void
+   */
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    // 마지막 스텝이 아니면 onNext 실행
+    if (step !== STEPS.PRICE) {
+      return onNext();
+    }
+
+    setIsLoading(true);
+    // post 제출
+    axios
+      .post('/api/listings', data)
+      .then(() => {
+        toast.success('Listing Created!');
+        router.refresh();
+        // 폼 제출후 모든 폼 리셋 react-hook-form
+        reset();
+        setStep(STEPS.CATEGORY);
+        rentModal.onClose();
+      })
+      .catch(() => {
+        toast.error('Something went wrong');
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   const actionLabel = useMemo(() => {
@@ -158,12 +198,79 @@ function RentModal() {
     );
   }
 
+  // 4번째 스텝
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Add a photo of your place'
+          subtitle='Show guests what your place looks like'
+        />
+        {/* 이미지 업로드 컴포넌트 */}
+        <ImageUpload
+          value={imageSrc}
+          onChange={(value) => setCustomValue('imageSrc', value)}
+        />
+      </div>
+    );
+  }
+
+  // 5번째 스텝
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='How would you describe your place?'
+          subtitle='Short and sweet works best!'
+        />
+        <Input
+          id='title'
+          label='Title'
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <hr />
+        <Input
+          id='description'
+          label='Description'
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.PRICE) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Now, set your price'
+          subtitle='How much do you charge per night'
+        />
+        <Input
+          id='price'
+          label='Price'
+          formatPrice
+          type='number'
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    );
+  }
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
       title='Airbnb your home'
       onClose={rentModal.onClose}
-      onSubmit={onNext}
+      onSubmit={handleSubmit(onSubmit)}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
