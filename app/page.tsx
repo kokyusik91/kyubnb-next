277e6aceb2427c75afde80c4e0a62a5681733a13
kyusikko <kokyusik91@naver.com>
2023-09-23 14:23:47 +0900

    🏷️ Date 타입을 string 타입으로 변경

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import ClientOnly from './components/ClientOnly';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
 import ListingCard from './components/listings/ListingCard';
+import { safeListings } from './types';
 
 export default async function Home() {
   const listings = await getListings();
@@ -24,7 +25,7 @@ export default async function Home() {
     <ClientOnly>
       <Container>
         <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-          {listings.map((listing) => {
+          {listings.map((listing: safeListings) => {
             return (
               <ListingCard
                 currentUser={currentUser}
