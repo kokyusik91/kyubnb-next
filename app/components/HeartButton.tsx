050be083cd99234c8ed2500c39167629ccf7ca4f
kyusikko <kokyusik91@naver.com>
2023-09-23 14:02:11 +0900

    ✨ 좋아요, 좋아요 취소 기능

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,6 +2,7 @@
 
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 import { SafeUser } from '../types';
+import useFavorite from '../hooks/useFavorite';
 
 interface HeartButtonProps {
   listingId: string;
@@ -9,9 +10,10 @@ interface HeartButtonProps {
 }
 
 function HeartButton({ listingId, currentUser }: HeartButtonProps) {
-  const hasFavorited = false;
-
-  const toggleFavorite = () => {};
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser,
+  });
   return (
     <div
       onClick={toggleFavorite}
---
+++ app/hooks/useFavorite.ts
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import React, { useCallback, useMemo } from 'react';
+
+import { toast } from 'react-hot-toast';
+
+import { SafeUser } from '../types';
+
+import useLoginModal from './useLoginModal';
+
+interface IUseFavorite {
+  listingId: string;
+  currentUser?: SafeUser | null;
+}
+
+const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
+  const router = useRouter();
+
+  const loginModal = useLoginModal();
+
+  const hasFavorited = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+
+    return list.includes(listingId);
+  }, [currentUser, listingId]);
+
+  const toggleFavorite = useCallback(
+    async (e: React.MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation();
+
+      // 만약에 user 정보가 없다면?? 로그인 모달을 띄운다.
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+
+      try {
+        let request;
+
+        if (hasFavorited) {
+          request = () => axios.delete(`/api/favorites/${listingId}`);
+        } else {
+          request = () => axios.post(`/api/favorites/${listingId}`);
+        }
+
+        await request();
+        router.refresh();
+        toast.success('Success');
+      } catch (error) {
+        toast.error('Something went wrong');
+      }
+    },
+    [currentUser, hasFavorited, listingId, loginModal, router]
+  );
+
+  return {
+    hasFavorited,
+    toggleFavorite,
+  };
+};
+
+export default useFavorite;
